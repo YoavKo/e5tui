8d18f506d0511b6617129aa1559ca0fa8caba3e2
idonacmias <idonacmi@gmail.com>
2024-09-06 11:06:17 +0300

    crawler warking on {list-of-all-pages}, add gitignore for htmls folder

--- crawler.py
+++ crawler.py
@@ -1,8 +1,30 @@
-
+import requests 
+import sys
+from bs4 import BeautifulSoup
+import time
 
 def main():
-    pass
+    for i in range(1,15):
+        html = read_from_site_to_text(f'system:list-all-pages/p/{i}')
+        soup = BeautifulSoup(html, 'html.parser')
+        main_content = soup.select_one('.main-content')
+        url_list = main_content.select_one('.list-pages-box').find_next("ul")
+        for link in url_list.find_all('a', href=True):
+            url = link['href']
+            html_file = read_from_site_to_text(url)
+            save_text_to_file(html_file, f'htmls/{url}.html')
+            print(url)
+            time.sleep(2)
 
+def read_from_site_to_text(content_name : str) -> str:
+    url=f'http://dnd5e.wikidot.com/{content_name}'
+    contents = requests.get(url)
+    return contents.text
+
+
+def save_text_to_file(text, file_name: str='11.html') -> None:
+    with open(file_name, 'w') as f:
+        f.write(text)
 
 
 if __name__ == '__main__':
