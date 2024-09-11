4c76514763b14160bf5bb918a9212af8b9b6fd1b
Yoav Kostiner <yoavko204@gmail.com>
2024-09-11 19:52:48 +0300

    tb POC cleanup

--- tb.py
+++ tb.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from tabulate import tabulate as tb 
 
-df = pd.read_html('11.html')
-print(type(df))
-#print(df.valuse.tolist())
-print(tb(df[0], headers='keys', tablefmt='psql'))
+dfs = pd.read_html('11.html')
+data = dfs[0]
+print(tb(data, headers='keys', tablefmt='psql'))
